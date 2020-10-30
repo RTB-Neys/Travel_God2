c88d82297e497224dc7724b6b0840d83c60db0d9
Neysalee Acosta <nacosta@clickpay.com>
2020-10-30 19:10:43 -0400

    TicketMaster for loop fix

--- script.js
+++ script.js
@@ -1,187 +1,140 @@
 //MAP STUFF
-
-//MAP STUFF
 let map;
 
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
-    });
-    const input = document.getElementById("pac-input");
-    const searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(input);
-    // Bias the SearchBox results towards current map's viewport.
-    map.addListener("bounds_changed", () => {
-        searchBox.setBounds(map.getBounds());
-    });
-    let markers = [];
-    // Listen for the event fired when the user selects a prediction and retrieve
-    // more details for that place.
-    searchBox.addListener("places_changed", () => {
-        const places = searchBox.getPlaces();
-        console.log(places)
-        if (places.length == 0) {
-            return;
-        }
-        // Clear out the old markers.
-        markers.forEach((marker) => {
-            marker.setMap(null);
+var currentLocation;
+
+
+    function initMap() {
+        map = new google.maps.Map(document.getElementById("map"), {
+            center: { lat: 40.7306, lng: -73.9352 },
+            zoom: 12,
         });
-        markers = [];
-        // For each place, get the icon, name and location.
-        const bounds = new google.maps.LatLngBounds();
-        places.forEach((place) => {
-            if (!place.geometry) {
-                console.log("Returned place contains no geometry");
+        const input = document.getElementById("travel-input");
+        const searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(input);
+        // Bias the SearchBox results towards current map's viewport.
+        map.addListener("bounds_changed", () => {
+            searchBox.setBounds(map.getBounds());
+        });
+        let markers = [];
+        // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+        searchBox.addListener("places_changed", () => {
+            const places = searchBox.getPlaces();
+            console.log(places) //where the name is captured 
+            currentLocation = places[0].name; //storing the name
+            console.log(currentLocation);
+            if (places.length == 0) {
                 return;
             }
-            const icon = {
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(25, 25),
-            };
-            // Create a marker for each place.
-            markers.push(
-                new google.maps.Marker({
-                    map,
-                    icon,
-                    title: place.name,
-                    position: place.geometry.location,
-                })
-            );
-
-            if (place.geometry.viewport) {
-                // Only geocodes have viewport.
-                bounds.union(place.geometry.viewport);
-            } else {
-                bounds.extend(place.geometry.location);
-            }
+            // Clear out the old markers.
+            markers.forEach((marker) => {
+                marker.setMap(null);
+            });
+            markers = [];
+            // For each place, get the icon, name and location.
+            const bounds = new google.maps.LatLngBounds();
+            latlngStore.push(bounds);
+            places.forEach((place) => {
+                if (!place.geometry) {
+                    console.log("Returned place contains no geometry");
+                    return;
+                }
+                const icon = {
+                    url: place.icon,
+                    size: new google.maps.Size(71, 71),
+                    origin: new google.maps.Point(0, 0),
+                    anchor: new google.maps.Point(17, 34),
+                    scaledSize: new google.maps.Size(25, 25),
+                };
+                // Create a marker for each place.
+                markers.push(
+                    new google.maps.Marker({
+                        map,
+                        icon,
+                        title: place.name,
+                        position: place.geometry.location,
+                    })
+                );
+                if (place.geometry.viewport) {
+                    // Only geocodes have viewport.
+                    bounds.union(place.geometry.viewport);
+                } else {
+                    bounds.extend(place.geometry.location);
+                }
+            });
+            map.setOptions({ minZoom: 12, maxZoom: 16 }) //fixed zoom to make more sense on search of city
+            map.fitBounds(bounds);
         });
-        map.fitBounds(bounds);
-    });
-}
+    }
 /////////////////
 
-// var getEvents = [];
+var latlngStore = [];
+console.log(latlngStore)
 
-// $.ajax({
-//     type: "GET",
-//     url: "https://app.ticketmaster.com/discovery/v2/events.json?size=4&apikey=ElWPP9FatyxVq4ke0f4mPT8u3LtGG04m",
-//     async: true,
-//     dataType: "json",
-//     success: function (json) {
-//         getEvents.json = json;
-//         showEvents(json);
-//         console.log(json);
-//     }
-// });
-
- 
-// function showEvents(json) {
-//     var items = $('#events .list-group-item');
-//     var events = json._embedded.events;
-//     var item = items.first();
-//     for (var i = 0; i < events.length; i++) {
-//         item.children('.list-group-item-heading').text(events[i].name);
-//         item.children('.list-group-item-text').text(events[i].dates.start.localDate);
-//         item.children('.list-group-url').text(events[i].url);
-//         try {
-//             item.children('.venue').text(events[i]._embedded.venues[0].name + " in " + events[i]._embedded.venues[0].city.name + " url " + events[i]._embedded.venues[0].url);
-//         } catch (err) {
-//             console.log(err);
-//         }
-//     }
-// }
-
-///
-/////////////////
+const search = document.querySelector("#travel-input");
+search.addEventListener("click", () => {
+	console.log("clicked search");
+});
 
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
+        navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-        var x = document.getElementById("location");
-        x.innerHTML = "Geolocation is not supported by this browser.";
+        
     }
 }
+
 function showPosition(position) {
-    var x = document.getElementById("location");
-    x = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
-    
     var latlon = position.coords.latitude + "," + position.coords.longitude;
 
+    var getEvents = [];
+
+
     $.ajax({
-      type:"GET",
-      url:"https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=ElWPP9FatyxVq4ke0f4mPT8u3LtGG04m&latlong + lat: , lng: ",
-      async:true,
-      dataType: "json",
-      success: function(json) {
-                  console.log(json);
-                  var e = document.getElementById("events");
-                  e.innerHTML = json.page.totalElements + " events found.";
-                  showEvents(json);
-                  initMap(position, json);
-               },
-      error: function(xhr, status, err) {
-                  console.log(err);
-               }
+        type: "GET",
+        url: "https://app.ticketmaster.com/discovery/v2/events.json?size=4&apikey=ElWPP9FatyxVq4ke0f4mPT8u3LtGG04m&latlong=" + latlon,
+        async: true,
+        dataType: "json",
+        success: function (json) {
+            getEvents.json = json;
+            showEvents(json);
+            console.log(json);
+        },
+        error: function (xhr, status, err) {
+            console.log(err);
+        }
     });
-    
+
+    // function showEvents(json) {
+    //     var items = $('#events .card');
+    //     var events = json._embedded.events;
+    //     var item = items.first();
+    //     for (var i = 0; i < events.length; i++) {
+    //         $(".card-event").append("<div>"+json._embedded.events[i].name+"</div>");
+    //         $(".card-content-event").append("<div>"+json._embedded.events[i].dates.start.localDate+"</div>");
+    //         $(".card-action-event").append("<div>"+json._embedded.events[i].url+"</div>");
+    //     }
+        
+    // }
+
+    function showEvents(json) {
+        var items = $('#events .card');
+        var events = json._embedded.events;
+        var item = items.first();
+        for (var i = 0; i < events.length; i++) {
+            var newDiv = $("<div>")
+            var pName = $("<p>").text(json._embedded.events[i].name);
+            var pDate = $("<p>").text(json._embedded.events[i].dates.start.localDate);
+            var pLink = $("<p>").text(json._embedded.events[i].url);
+            newDiv.append(pName, pDate, pLink);
+
+            $("#card-content-event" + [i]).append(newDiv)
+        }
+    };
+
 }
-
-function showError(error) {
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-            x.innerHTML = "User denied the request for Geolocation."
-            break;
-        case error.POSITION_UNAVAILABLE:
-            x.innerHTML = "Location information is unavailable."
-            break;
-        case error.TIMEOUT:
-            x.innerHTML = "The request to get user location timed out."
-            break;
-        case error.UNKNOWN_ERROR:
-            x.innerHTML = "An unknown error occurred."
-            break;
-    }
-}
-
-
-function showEvents(json) {
-  for(var i=0; i<json.page.size; i++) {
-    $("#events").append("<p>"+json._embedded.events[i].name+"</p>");
-  }
-}
-
-
-function initMap(position, json) {
-  var mapDiv = document.getElementById('map');
-  var map = new google.maps.Map(mapDiv, {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
-  });
-  for(var i=0; i<json.page.size; i++) {
-    addMarker(map, json._embedded.events[i]);
-  }
-}
-
-function addMarker(map, event) {
-  var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(event._embedded.venues[0].location.latitude, event._embedded.venues[0].location.longitude),
-    map: map
-  });
-  marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
-  console.log(marker);
-}
-
-
-
 
 getLocation();
 
-
-
-
+////
